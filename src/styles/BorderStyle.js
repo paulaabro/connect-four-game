3247062dcab5d3c3d14d24e4797930db96b2ca2b
Tissyane <tissyane@gmail.com>
2023-05-12 16:33:44 -0300

    Removed px from zero

--- src/styles/BorderStyle.js
+++ src/styles/BorderStyle.js
@@ -2,6 +2,6 @@ import { styled } from "styled-components";
 
 export const BorderStyle = styled.div`
   border: 3px solid ${props => props.theme.black};
-  box-shadow: 0 10px 0px ${props => props.theme.black};
+  box-shadow: 0 10px 0 ${props => props.theme.black};
   border-radius: 40px;
 `;
