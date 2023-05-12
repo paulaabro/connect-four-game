51587143db3d4e830fc994e9af567a22734e8242
Tissyane <tissyane@gmail.com>
2023-05-12 14:29:19 -0300

    feat: rules - Created Rules Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import { ThemeProvider } from "styled-components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MenuPage from "./pages/MenuPage";
 import GamePage from "./pages/GamePage";
 import RulesPage from "./pages/RulesPage";
 
-import { THEME } from "./themes/theme";
-
 export default function App() {
   return (
-    <ThemeProvider theme={THEME}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MenuPage />} />
-          <Route path="/game" element={<GamePage />} />
-          <Route path="/rules" element={<RulesPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MenuPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/rules" element={<RulesPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
